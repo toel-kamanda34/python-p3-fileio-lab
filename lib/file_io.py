41a5093d272eb5862d0ca4ebe81ec375b45b3b97
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-12 11:41:05 +0300

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt",mode='w',encoding='utf-8') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt",mode='a',encoding='utf-8') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt",mode='r', encoding='utf-8') as text_file:
+        return text_file.read()
+
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+content = read_file(file_name="logfile")
+print(content)
